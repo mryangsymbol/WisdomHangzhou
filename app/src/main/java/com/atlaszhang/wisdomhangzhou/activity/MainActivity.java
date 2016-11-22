bb83cebe70c46dcfc413d301f8a6f7cda5c8ed1f
zhangzhian <zhangzhian_123@qq.com>
2016-11-23 00:05:12 +0800

    添加侧滑栏

--- app/src/main/java/com/atlaszhang/wisdomhangzhou/activity/MainActivity.java
+++ app/src/main/java/com/atlaszhang/wisdomhangzhou/activity/MainActivity.java
@@ -1,24 +1,32 @@
 package com.atlaszhang.wisdomhangzhou.activity;
 
-import android.app.Activity;
 import android.os.Bundle;
 import android.view.Window;
 
 import com.atlaszhang.wisdomhangzhou.R;
+import com.jeremyfeinstein.slidingmenu.lib.SlidingMenu;
+import com.jeremyfeinstein.slidingmenu.lib.app.SlidingFragmentActivity;
 
 /**
  * 主页面
- * 
+ *
  * @author Kevin
  * @date 2015-10-17
  */
-public class MainActivity extends Activity {
+public class MainActivity extends SlidingFragmentActivity {
 
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		requestWindowFeature(Window.FEATURE_NO_TITLE);// 去掉标题,
-//		// 必须在setContentView之前调用
-		setContentView(R.layout.activity_main);
-	}
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);// 去掉标题,
+        // 必须在setContentView之前调用
+        setContentView(R.layout.activity_main);
+
+        //Utils.doSomthing();
+        //R.drawable.p_10
+        setBehindContentView(R.layout.left_menu);
+        SlidingMenu slidingMenu = getSlidingMenu();
+        slidingMenu.setTouchModeAbove(SlidingMenu.TOUCHMODE_FULLSCREEN);//全屏触摸
+        slidingMenu.setBehindOffset(500);//屏幕预留200像素宽度
+    }
 }
